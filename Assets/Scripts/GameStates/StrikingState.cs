6934bdb469ee47c9c434e363d052fe43b9670e5b
tuorai <tubackkhoa@gmail.com>
2021-08-13 11:17:17 +0700

    initial commit

---
+++ Assets/Scripts/GameStates/StrikingState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameStates
+{
+    public class StrikingState : AbstractGameObjectState
+    {
+        private PoolGameController gameController;
+
+        private GameObject cue;
+        private GameObject cueBall;
+
+        private float cueDirection = -1;
+        private float speed = 7;
+
+        public StrikingState(MonoBehaviour parent) : base(parent)
+        {
+            gameController = (PoolGameController)parent;
+            cue = gameController.cue;
+            cueBall = gameController.cueBall;
+        }
+
+        public override void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.touches[0];
+                switch (touch.phase)
+                {
+                    case TouchPhase.Moved:
+                    case TouchPhase.Canceled:
+                        // if still move then waiting for next turn
+                        gameController.currentState = new GameStates.WaitingForStrikeState(gameController);
+                        break;
+
+                    case TouchPhase.Ended:
+                        // then strike
+                        gameController.currentState = new GameStates.StrikeState(gameController);
+                        break;
+                }
+            }
+
+        }
+
+        public override void FixedUpdate()
+        {
+            var distance = Vector3.Distance(cue.transform.position, cueBall.transform.position);
+            if (distance < PoolGameController.MIN_DISTANCE || distance > PoolGameController.MAX_DISTANCE)
+                cueDirection *= -1;
+            cue.transform.Translate(Vector3.down * speed * cueDirection * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/GameStates/WaitingForStrikeState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GameStates
+{
+    public class WaitingForStrikeState : AbstractGameObjectState
+    {
+        private GameObject cue;
+        private GameObject cueBall;
+        private GameObject mainCamera;
+
+        private PoolGameController gameController;
+
+        public WaitingForStrikeState(MonoBehaviour parent) : base(parent)
+        {
+            gameController = (PoolGameController)parent;
+            cue = gameController.cue;
+            cueBall = gameController.cueBall;
+            mainCamera = gameController.mainCamera;
+
+            cue.GetComponent<Renderer>().enabled = true;
+        }
+
+        public override void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                var touch = Input.touches[0];
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+
+                        gameController.pressPosX = Input.mousePosition.x;
+                        break;
+
+                    case TouchPhase.Moved:
+                        var x = (Input.touches[0].position.x - gameController.pressPosX) / 2;
+                        if (x != 0)
+                        {
+                            var angle = x * Time.deltaTime;
+                            gameController.strikeDirection = Quaternion.AngleAxis(angle, Vector3.up) * gameController.strikeDirection;
+                            mainCamera.transform.RotateAround(cueBall.transform.position, Vector3.up, angle);
+                            cue.transform.RotateAround(cueBall.transform.position, Vector3.up, angle);
+                        }
+                        break;
+
+                    case TouchPhase.Stationary:
+                        gameController.currentState = new GameStates.StrikingState(gameController);
+                        break;
+                }
+            }
+
+            Debug.DrawLine(cueBall.transform.position, cueBall.transform.position + gameController.strikeDirection * 10);
+
+        }
+    }
+}
